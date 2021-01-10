c2b8d3ae30c60fbaba3219ea0722707c78e3c000
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-01-10 15:03:26 +0300

    Updated calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
 // Define the numbers and the operation here
 
+const firstNumber = 5;
+const secondNumber = 8;
+const operation = "+";
+
 // Write if-statements here
+
+if (operation === "+") {
+  const result = firstNumber + secondNumber;
+  console.log(result);
+} else if (operation === "-") {
+  const result = firstNumber - secondNumber;
+  console.log(result);
+} else if (operation === "*") {
+  const result = firstNumber * secondNumber;
+  console.log(result);
+} else if (operation === "/") {
+  const result = firstNumber / secondNumber;
+  console.log(result);
+} else {
+  console.log("You have entered an invalid operator.");
+}
